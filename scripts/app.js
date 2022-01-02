0d0c62b17d149089deafeb20ff86125a0154c773
MertPlc <mertpalacioglu@gmail.com>
2022-01-02 19:33:09 +0300

    weather icons & images

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
   //   const cityDets = data.cityDets;
@@ -8,7 +10,7 @@ const updateUI = (data) => {
 
   // destructure properties
   const { cityDets, weather } = data;
-  
+
   // update details template
   details.innerHTML = `
     <h5 class="my-3">${cityDets.EnglishName}</h5>
@@ -18,6 +20,13 @@ const updateUI = (data) => {
         <span>&deg;C</span>
     </div>
     `;
+
+  // update the night/day & icon images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+  time.setAttribute("src", timeSrc);
 
   // remove the d-none class if present
   if (card.classList.contains("d-none")) {
